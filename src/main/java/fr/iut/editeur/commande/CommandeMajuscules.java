dd10ff1109925709a7e191eec86cdccfab30d79c
Sacha <sacha2005.sd@gmail.com>
2024-09-12 09:46:57 +0200

    test

--- src/main/java/fr/iut/editeur/commande/CommandeMajuscules.java
+++ src/main/java/fr/iut/editeur/commande/CommandeMajuscules.java
@@ -2,21 +2,21 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
-public class CommandeEffacer extends CommandeDocument {
+public class CommandeMajuscules extends CommandeDocument {
 
-    public CommandeEffacer(Document document, String[] parameters) {
+    public CommandeMajuscules(Document document, String[] parameters) {
         super(document, parameters);
     }
 
     @Override
     public void executer() {
         if(parameters.length < 2) {
-            System.err.println("Format attendu :  effacer;debut;fin");
+            System.err.println("Format attendu :  majuscules;debut;fin");
             return;
         }
         int debut = Integer.parseInt(parameters[1]);
         int fin = Integer.parseInt(parameters[2]);
-        this.document.effacer(debut,fin);
+        this.document.majuscules(debut,fin);
         super.executer();
     }
 
